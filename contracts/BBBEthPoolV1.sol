2446da81f847f49bce2a25fe32d889aa212e9eff
Theo Ilie <theoilie.ti@gmail.com>
2022-01-06 18:02:36 -0800

    Allow unlimited staking period and fill in some pool contract functions

--- contracts/BBBEthPoolV1.sol
+++ contracts/BBBEthPoolV1.sol
@@ -4,29 +4,76 @@ pragma solidity ^0.8.11;
 import "./interfaces/IBBBPoolV1.sol";
 
 /// Pool for providing ether liquidity for Arbitrum fast withdrawals.
-// TODO
 contract BBBEthPoolV1 is IBBBPoolV1 {
-    function distributeFee(uint256 amount) external override {
+    struct Balance {
+        uint staked;
+        UnstakingBalance[] unstaking;
+    }
 
+    struct UnstakingBalance {
+        uint amount;
+        uint timestampUnlocked;
+    }
+
+    mapping(address => Balance) balances;
+    uint availableLiq;
+
+    function distributeFee(uint amount) external override {
+        // TODO
      }
 
-    function provideLiq15Days(uint256 amount) external override {
-
+    function provideLiq() external payable override {
+        Balance storage balance = balances[msg.sender];
+        balance.staked += msg.value;
+        availableLiq += msg.value;
     }
 
-    function withdrawLiq(address farmer, uint256 amount) external override {
+    function unstake(uint amount) external override {
+        Balance storage balance = balances[tx.origin];
 
-    }
-    
-    function getTotalBalance(address farmer) external view override returns (uint256) {
+        require(balance.staked >= amount, "Amount requested exceeds staked balance");
 
-    }
-
-    function getWithdrawableBalance(address farmer) external view override returns (uint256) {
-
+        balance.staked -= amount;
+        balance.unstaking.push(UnstakingBalance(amount, block.timestamp + 7 days));
     }
 
-    function getTotalLiq() external view override returns (uint256) {
+    function withdrawBalance(uint amount) external override {
+        // TODO: Withdraw as many elements of balances[tx.sender].unstaking as needed
+    }
 
+    function getTotalBalance(address farmer) external view override returns (uint) {
+        return this.getStakedBalance(farmer) + this.getUnstakedBalance(farmer) + this.getWithdrawableBalance(farmer);
+    }
+
+    function getStakedBalance(address farmer) external view override returns (uint) {
+        return balances[farmer].staked;
+    }
+
+    function getUnstakedBalance(address farmer) external view override returns (uint unstaked_) {
+        Balance storage balance = balances[farmer];
+        for (uint i = 0; i < balance.unstaking.length; i++) {
+            UnstakingBalance storage unstaking = balance.unstaking[i];
+
+            // Balance is still unstaking and not withdrawable until the current blocktime is at least `timestampUnlocked`
+            if (block.timestamp < unstaking.timestampUnlocked) {
+                unstaked_ += unstaking.amount;
+            }
+        }
+    }
+
+    function getWithdrawableBalance(address farmer) external view override returns (uint withdrawable_) {
+        Balance storage balance = balances[farmer];
+        for (uint i = 0; i < balance.unstaking.length; i++) {
+            UnstakingBalance storage unstaking = balance.unstaking[i];
+
+            // Balance is withdrawable once the current blocktime is at least `timestampUnlocked`
+            if (block.timestamp >= unstaking.timestampUnlocked) {
+                withdrawable_ += unstaking.amount;
+            }
+        }
+    }
+
+    function getAvailableLiq() external view override returns (uint) {
+        return availableLiq;
     }
 }
--- contracts/interfaces/IBBBPoolV1.sol
+++ contracts/interfaces/IBBBPoolV1.sol
@@ -14,37 +14,41 @@ interface IBBBPoolV1 {
      * @notice Distributes a fee (paid by withdrawer) to stakers, in proportion to each staker's stake
      * @param amount The total fee, in ether, to distribute
      */
-    function distributeFee(uint256 amount) external;
+    function distributeFee(uint amount) external;
 
-    /**
-     * @notice Stakes ether for 15 days, locking it up for the full 15 days if someone
-     *        uses it for a fast withdrawal, or for only 8 days if no one uses it.
-     * @param amount The amount of ether to stake
-     */
-    function provideLiq15Days(uint256 amount) external;
+    /// @notice Stake ether. Unstaking takes 7 days.
+    function provideLiq() external payable;
 
-    /**
-     * @notice Unstakes ether if it's locked for <7 days.
-     * @param farmer The address of the wallet who staked the ether
-     * @param amount The amount of ether to unstake
-     */
-    function withdrawLiq(address farmer, uint256 amount) external;
+    /// Start unstaking `amount` ether. Will be available to withdraw in 7 days.
+    function unstake(uint amount) external;
+
+    /// Withdraw `amount` ether.
+    function withdrawBalance(uint amount) external;
     
     /**
      * @param farmer The address of the wallet to view total ether staked
+     * @return Amount of ether that the address has in the pool, including ether that is unstaked or in the process of unstaking
+     */
+    function getTotalBalance(address farmer) external view returns (uint);
+
+    /**
+     * @param farmer The address of the wallet to view staked ether
      * @return Amount of ether that the address has staked
      */
-    function getTotalBalance(address farmer) external view returns (uint256);
+    function getStakedBalance(address farmer) external view returns (uint);
 
     /**
-     * @param farmer The address of the wallet to view withdrawable ether staked
-     * @return Amount of withdrawable (not locked) ether that the address has staked
+     * @param farmer The address of the wallet to view unstaked ether
+     * @return Amount of ether that the address has started unstaking but cannot withdraw yet
      */
-    function getWithdrawableBalance(address farmer) external view returns (uint256);
+    function getUnstakedBalance(address farmer) external view returns (uint);
 
     /**
-     * @notice This excludes liquidity that is locked for <7 days.
-     * @return Amount of ether available in the pool for fast withdrawals
+     * @param farmer The address of the wallet to view withdrawable ether
+     * @return Amount of ether that the address has unstaked and can withdraw
      */
-    function getTotalLiq() external view returns (uint256);
+    function getWithdrawableBalance(address farmer) external view returns (uint);
+
+    /// Get the pool's total amount of ether that's locked and ready to be used.
+    function getAvailableLiq() external view returns (uint);
 }
